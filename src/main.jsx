d2904861e82ec17fdacb97245cb403e5db1b7ff2
vittotaufiiqul <95577847+vittotaufiiqul@users.noreply.github.com>
2026-05-12 08:45:13 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './styles/global.css'
+import './styles/mobile.css' 
 import './styles/datepicker.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
